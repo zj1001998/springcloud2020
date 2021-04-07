f404ca46e01097f2fdbaa3ededb312f3c893c899
zj1001998@aliyun.com <zj1001998@aliyun.com>
2021-04-07 09:30:51 +0800

    第一次提交信息

---
+++ cloud-consumerZK-order80/src/main/java/com/atguigu/springcloud/OrderZkMain80.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author aj
+ * @create 2021-03-26 10:38
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderZkMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderZkMain80.class,args);
+    }
+}
